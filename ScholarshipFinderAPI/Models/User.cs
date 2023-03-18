76ca2774fa6192df9a3ce3b75f615561f4a55f0e
Yslamguly <islamguly28@gmail.com>
2023-03-18 15:59:06 +0100

    remove isVerified column from Users

--- ScholarshipFinderAPI/Models/User.cs
+++ ScholarshipFinderAPI/Models/User.cs
@@ -2,7 +2,6 @@ namespace ScholarhipFinderAPI.Models{
     public class User : BaseEntity{
         public string Email { get; set; }
         public string Password { get; set; }
-        public bool IsVerified { get; set; }
 
         public ICollection<WishList> WishLists { get; set; }
 
