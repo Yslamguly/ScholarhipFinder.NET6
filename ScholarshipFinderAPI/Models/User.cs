b0050024055b6c0c83407e578d346c0b9fbace86
Yslamguly <islamguly28@gmail.com>
2023-03-20 12:55:04 +0100

    added user name

--- ScholarshipFinderAPI/Models/User.cs
+++ ScholarshipFinderAPI/Models/User.cs
@@ -1,5 +1,8 @@
+using Microsoft.AspNetCore.Identity;
+
 namespace ScholarhipFinderAPI.Models{
     public class User : BaseEntity{
+        public string UserName { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
 
