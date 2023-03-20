53b939ea78b976003c017f14abd06a26a5d3b955
Yslamguly <islamguly28@gmail.com>
2023-03-20 12:54:00 +0100

    migration to add user name to users table

--- ScholarshipFinderAPI/Migrations/20230320103458_Adding user name to users table.Designer.cs
+++ ScholarshipFinderAPI/Migrations/20230320103458_Adding user name to users table.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using ScholarhipFinderAPI.Data;
@@ -11,9 +12,10 @@ using ScholarhipFinderAPI.Data;
 namespace ScholarshipFinderAPI.Migrations
 {
     [DbContext(typeof(ApiDbContext))]
-    partial class ApiDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230320103458_Adding user name to users table")]
+    partial class Addingusernametouserstable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
