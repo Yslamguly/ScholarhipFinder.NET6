c84907c3383de5bc1c8bfe57ffba5e90195e8f56
Yslamguly <islamguly28@gmail.com>
2023-03-22 09:34:19 +0100

    user class does not inherit from the BaseEntity class

--- ScholarshipFinderAPI/Models/User.cs
+++ ScholarshipFinderAPI/Models/User.cs
@@ -1,11 +1,13 @@
 using Microsoft.AspNetCore.Identity;
 
 namespace ScholarhipFinderAPI.Models{
-    public class User : BaseEntity{
+    public class User {
+        public string UserId { get; set; }
         public string UserName { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
-
+        public DateTime DateAdded { get; set; }
+		public DateTime DateUpdated { get; set; }
         public ICollection<WishList> WishLists { get; set; }
 
     }
