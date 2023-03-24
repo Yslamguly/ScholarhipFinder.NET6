403bc76fff4fe7f78ac196e97ab3e8c1946c512e
Yslamguly <islamguly28@gmail.com>
2023-03-24 12:52:32 +0100

    token manager class to make operations with tokens

---
+++ ScholarshipFinderAPI/Helpers/TokenManager.cs
@@ -0,0 +1,60 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+
+namespace ScholarhipFinderAPI.Helpers{
+    public class TokenManager{
+        public TokenManager(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        private readonly IConfiguration _configuration;
+        public ClaimsPrincipal ValidateToken(string token)
+        {
+            // var token = Request.Headers["Authorization"].FirstOrDefault()?.Split(" ").Last();
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_configuration.GetSection("JwtConfig:Secret").Value);
+
+
+            // Set the validation parameters
+            var validationParameters = new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(key),
+                ValidateIssuer = false,
+                ValidateAudience = false,
+                ClockSkew = TimeSpan.Zero
+            };
+
+            // Validate the token
+            var claimsPrincipal = tokenHandler.ValidateToken(token, validationParameters, out var validatedToken);
+
+            return claimsPrincipal;
+        }
+        public string GenerateToken(IdentityUser identityUser)
+        {
+            var jwtTokenHandler = new JwtSecurityTokenHandler();
+
+            var key = Encoding.UTF8.GetBytes(_configuration.GetSection("JwtConfig:Secret").Value);
+
+            var tokenDescriptor = new SecurityTokenDescriptor(){
+                Subject = new ClaimsIdentity(new []{
+                    new Claim("Id",identityUser.Id),
+                    new Claim(JwtRegisteredClaimNames.Sub,identityUser.Email),
+                    new Claim(JwtRegisteredClaimNames.Email,identityUser.Email),
+                    new Claim(JwtRegisteredClaimNames.Jti,Guid.NewGuid().ToString()),
+                    new Claim(JwtRegisteredClaimNames.Iat,DateTime.Now.ToUniversalTime().ToString()),
+                }),
+                Expires = DateTime.Now.AddHours(1),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key),SecurityAlgorithms.HmacSha256)
+            };
+
+            var token = jwtTokenHandler.CreateToken(tokenDescriptor);
+            var jwtToken = jwtTokenHandler.WriteToken(token); 
+
+            return jwtToken;
+        }
+    }
+}
